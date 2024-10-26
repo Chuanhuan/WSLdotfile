77cc1e1da582d88954291bb6b1bf6cf70ce732d5
chuanhuan <studyhuan@gmail.com>
2024-10-26 12:26:30 +0800

    10/26/24

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 
 vim.g.vimtex_view_general_viewer = "sumatraPDF"
+-- vim.g.vimtex_view_general_viewer = "Okular"
 vim.g.vimtex_view_general_options = "-reuse-instance @pdf"
 vim.g.vimtex_view_general_options_latexmk = "-reuse-instance"
 
